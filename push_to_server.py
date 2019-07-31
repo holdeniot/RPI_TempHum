f529f9bae01640beae79fc9f4c2291b62c072957
holdeniot <53030765+holdeniot@users.noreply.github.com>
2019-07-31 13:42:52 +1000

    Changes made to allow for 2nd Pi
    Added GUID parameter to the HTML String

--- push_to_server.py
+++ push_to_server.py
@@ -1,13 +1,13 @@
 import urllib.request
 
-def push_data(temp,hum):
+def push_data(guid,temp,hum):
 
-    url = urllib.request.urlopen("http://holdeniot.000webhostapp.com/rpi_uploads/temperature.php/?temp={0:0.01f}&hum={1:0.01f}&page=0".format(temp,hum))
+    url = urllib.request.urlopen("http://holdeniot.000webhostapp.com/rpi_uploads/temperature.php/?temp={0:0.01f}&hum={1:0.01f}&guid={2:1f}&page=0".format(temp,hum,guid))
 
     return
 
-def email_alert(temp,hum):
+def email_alert(guid,temp,hum):
     
-    url = urllib.request.urlopen("http://holdeniot.000webhostapp.com/emailAlert.php/?temp={0:0.01f}&hum={1:0.01f}".format(temp,hum))
+    url = urllib.request.urlopen("http://holdeniot.000webhostapp.com/emailAlert.php/?temp={0:0.01f}&hum={1:0.01f}&guid={2,1f}".format(temp,hum,guid))
     
     return
