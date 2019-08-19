4a442b320b8b535a31b979c2c4a86b2b1350d1a1
holdeniot <53030765+holdeniot@users.noreply.github.com>
2019-08-19 13:29:28 +1000

    Added a functionality to upload once a minute .

--- push_to_server.py
+++ push_to_server.py
@@ -5,6 +5,12 @@ def push_data(guid,temp,hum):
     url = urllib.request.urlopen("http://holdeniot.000webhostapp.com/rpi_uploads/temperature.php/?temp={0:0.01f}&hum={1:0.01f}&guid={2:1f}&page=0".format(temp,hum,guid))
 
     return
+	
+def push_data_minute(guid,temp,hum):
+
+    url = urllib.request.urlopen("http://holdeniot.000webhostapp.com/rpi_uploads/temperatureMinute.php/?temp={0:0.01f}&hum={1:0.01f}&guid={2:1f}&page=0".format(temp,hum,guid))
+
+    return	
 
 def email_alert(guid,temp,hum):
     
