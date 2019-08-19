af10c75c4a274fc607dca13846c7ec14d45a0cf2
holdeniot <53030765+holdeniot@users.noreply.github.com>
2019-08-19 14:43:43 +1000

    Updated the file to fix TRY CATCH statement

--- main_program_catch.py
+++ main_program_catch.py
@@ -8,8 +8,8 @@ from datetime import datetime
 LOG_FILENAME = datetime.now().strftime('rpi_logfile_%H_%M_%S_%d_%m_%Y.log')
 
 for handler in logging.root.handlers[:]:
-	logging.root.removeHandler(handler)
-logging.basicConfig(filename=LOG_FILENAME,level=logging.DEBUG)
+    logging.root.removeHandler(handler)
+logging.basicConfig(filename=LOG_FILENAME,level=logging.INFO)
 logging.info("RPi with GUID 2 Started")
 
 
@@ -25,16 +25,16 @@ time.sleep(5)
 
 #for i in range(1,300):
 while 1:
-	
+    
     try:
-	[temp,hum]= read_sensor.read_bme_sensor()
-    except Exception, error:
-	logging.info('Error in measurement: ')
-	logging.error(str(error))
+        [temp,hum]= read_sensor.read_bme_sensor()
+    except Exception as error:
+        logging.info('Error in measurement: ')
+        logging.error(str(error))
     else:
-	logging.info('Sensor measurement successful')
+        logging.info('Sensor measurement successful')
     finally:
-	logging.info('Finished measurement section')
+        logging.info('Finished measurement section')
     
     if (temp > max_temp):
         count_above_max_temp+=1
@@ -52,19 +52,19 @@ while 1:
     if (count_above_max_hum >= number_readings+1):
         push_to_server.email_alert(GUID,temp,hum)
     
-	
+    
     try:
-	push_to_server.push_data(GUID,temp,hum)
-    except Exception, error:
-	logging.info('Error in pushing data to server: ')
-	logging.error(str(error))
+        push_to_server.push_data(GUID,temp,hum)
+    except Exception as error:
+        logging.info('Error in pushing data to server: ')
+        logging.error(str(error))
     else:
-	logging.info('Data pushed successfully')
+        logging.info('Data pushed successfully')
     finally:
-	logging.info('Finished pushing data')
+        logging.info('Finished pushing data')
     
     time.sleep(600)
-	
+    
 logging.error("If you see this message, the program has exited the main loop.")
     
   
